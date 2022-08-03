f6513517ccdc7e6fdbb9542f27f607efeffcf256
senny <80088956+rnfltpgus@users.noreply.github.com>
2022-08-03 16:16:43 +0900

    Time: 204 ms (53.14%), Space: 46.9 MB (72.31%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+  const nums = {
+    "I": 1,
+    "V": 5,
+    "X": 10,
+    "L": 50,
+    "C": 100,
+    "D": 500,
+    "M": 1000
+  }
+  
+  let acc = 0;
+  
+  for (let i = 0; i < s.length; i += 1) {
+    if (nums[s[i]] < nums[s[i + 1]]) {
+      acc -= nums[s[i]];
+    } else {
+      acc += nums[s[i]];
+    }
+  }
+  
+  return acc;
+};
