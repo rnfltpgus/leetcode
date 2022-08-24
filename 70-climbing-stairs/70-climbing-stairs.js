a24a18028a958d5bf5deef942944c201ebd7d530
sany <80088956+rnfltpgus@users.noreply.github.com>
2022-08-24 22:27:30 +0900

    Time: 87 ms (48.54%), Space: 41.9 MB (44.23%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+const cache = {
+  "0": 1,
+  "1": 1,
+};
+
+var climbStairs = function(n) {
+    if (n < 3 ) {
+      return n;
+    }
+  
+    let arr = [1,2];
+  
+    for (let i = 2; i < n; i++) {
+      arr[i] = arr[i-1] + arr[i-2];
+    }
+  
+    return arr[arr.length-1];
+};
