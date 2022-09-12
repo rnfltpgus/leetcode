b13d97aeb856a61e40101a214df3b19d2565c7f4
sany <80088956+rnfltpgus@users.noreply.github.com>
2022-09-12 17:02:50 +0900

    Time: 112 ms (11.73%), Space: 42.3 MB (26.81%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.js
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.js
@@ -3,15 +3,15 @@
  * @return {number}
  */
 var numIdenticalPairs = function(nums) {
-  let result = 0
+  let result = 0;
   
   for (i = 0; i < nums.length - 1; i++) {
     for (j = i + 1; j < nums.length; j++) {
       if (nums[i] === nums[j]) {
-        result++
+        result++;
       }
     }
   }
   
-  return result
+  return result;
 };
