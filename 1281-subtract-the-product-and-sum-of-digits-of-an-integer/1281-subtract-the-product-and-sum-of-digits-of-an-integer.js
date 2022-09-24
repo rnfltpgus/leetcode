3ab983d19bdd78e71c08500bf66cf29068325c4e
sany <80088956+rnfltpgus@users.noreply.github.com>
2022-09-24 21:41:24 +0900

    Time: 91 ms (50.20%), Space: 42.2 MB (51.47%) - LeetHub

---
+++ 1281-subtract-the-product-and-sum-of-digits-of-an-integer/1281-subtract-the-product-and-sum-of-digits-of-an-integer.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var subtractProductAndSum = function(n) {
+  let sum = 0;
+  let product = 1;
+
+  while (n > 0) {
+    let digit = n % 10;
+    
+    sum += digit;
+    product *= digit;
+    n = Math.floor(n / 10);
+  }
+
+  return product - sum;    
+};
