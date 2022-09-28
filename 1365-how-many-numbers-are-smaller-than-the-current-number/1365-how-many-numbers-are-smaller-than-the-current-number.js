39abc90a1fc9eb72d62e6915b847fe71369ee963
sany <80088956+rnfltpgus@users.noreply.github.com>
2022-09-28 21:16:09 +0900

    Time: 98 ms (82.04%), Space: 44.4 MB (40.07%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.js
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.js
@@ -3,5 +3,9 @@
  * @return {number[]}
  */
 var smallerNumbersThanCurrent = function (nums) {
+  if (nums.length === 0) {
+    return;      
+  }
+  
   return nums.map((n) => nums.reduce((a, b) => a + (n > b ? 1 : 0), 0));
 };
